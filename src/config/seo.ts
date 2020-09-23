b3f459b35126937eb1ed77c1719d032576ab55f5
luminuszz <luminuszz43@gmail.com>
2020-09-23 20:40:54 -0300

    new

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -4,13 +4,13 @@ const SEO: DefaultSeoProps = {
   title: 'Davi Ribeiro | luminuszz',
 
   description:
-    'Welcome  This is my little space | Bem vindo, Esse é o meu pequeno espaço,',
+    'luminuszz.dev, Welcome  This is my little space | Bem vindo, Esse é o meu pequeno espaço',
 
   twitter: {
     site: 'https://twitter.com/luminuszz1',
   },
 
-  titleTemplate: 'Davi Ribeiro | luminuszz',
+  titleTemplate: 'Luminuszz',
 };
 
 export default SEO;
