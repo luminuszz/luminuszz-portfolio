948c6010ca2ecdc212e416ee8e54999d733c40e8
luminuszz <luminuszz43@gmail.com>
2020-09-23 19:41:44 -0300

    new

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,31 +1,74 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './styles.scss';
+import { BsArrowLeft } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { SiGmail } from 'react-icons/si';
 
 const Navbar: React.FC = () => {
+  const headerRef = useRef<HTMLHeadingElement>(null);
   const [isSticky, setIsSticky] = useState(false);
+  const [openNav, setOpenNav] = useState(false);
+
+  const handleNavBarSticky = useCallback(() => {
+    setIsSticky(window.pageYOffset > headerRef.current?.offsetTop);
+  }, [headerRef]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleNavBarSticky);
+  }, [handleNavBarSticky]);
+
+  const handleControlNavarbar = useCallback(() => {
+    setOpenNav(state => !state);
+    document.querySelector('body').classList.toggle('removeScroll', !openNav);
+  }, [openNav]);
 
   return (
-    <header>
-      <div className="header-content">
-        <div className="logo">
-          <h3>DAVI RIBEIRO</h3>
+    <>
+      <header ref={headerRef} className={`${isSticky && 'is-Sticky'}`}>
+        <div className="header-content">
+          <div className="logo is-flex ">
+            <GiHamburgerMenu
+              className="is-hidden-desktop"
+              onClick={handleControlNavarbar}
+            />
+            <h3>DAVI RIBEIRO</h3>
+          </div>
+
+          <div className="menu-links  is-hidden-mobile">
+            <a href="#about">About Me</a>
+            <a href="#about">Stack</a>
+
+            <a
+              className="mailto"
+              href="mailto:davi5.ribeiro.contanto@gmail.com?subject='Olá'"
+            >
+              <SiGmail />
+              Send a message
+            </a>
+          </div>
         </div>
 
-        <div className={`menu-links ${isSticky && 'is-Sticky'}`}>
-          <a href="#about">About Me</a>
-          <a href="#about">Projects</a>
+        <div className={`menu-mobile ${openNav && 'active'}`}>
+          <div className="exit-icon">
+            <BsArrowLeft onClick={handleControlNavarbar} />
+          </div>
 
-          <a
-            className="mailto"
-            href="mailto:davi5.ribeiro.contanto@gmail.com?subject='Olá'"
-          >
-            <SiGmail />
-            Send a message
-          </a>
+          <div className="menu-links is-hidden-desktop">
+            <a href="#about">About Me</a>
+            <a href="#about">Stack</a>
+
+            <a
+              className="mailto"
+              href="mailto:davi5.ribeiro.contanto@gmail.com?subject='Olá'"
+            >
+              <SiGmail />
+              Send a message
+            </a>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {openNav && <div className="set-opacity" />}
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
 import { NextPage } from 'next';
 import React, { useEffect, useRef } from 'react';
+import { DiMongodb, DiPostgresql } from 'react-icons/di';
 import { FaReact } from 'react-icons/fa';
 import { GrNode } from 'react-icons/gr';
+import { SiTypescript } from 'react-icons/si';
 
 import profile from '../assets/profile.jpeg';
+import Footer from '../components/Footer';
 import handleAnimationWord from '../utils/handleAnimationWord';
 
 const Home: NextPage = () => {
@@ -16,14 +19,17 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <section className="hero">
+      <section className="hero section-main">
         <div className="hero-body">
           <div className="container">
             <div className="level">
               <div className="level-left">
-                <div className="level-item">
-                  <p className="content is-family-primary ">
-                    <span className="is-size-4 animation-bar " ref={textRef}>
+                <div className="level-item has-text-centered-mobile ">
+                  <p className="content is-family-primary is-size-3-mobile is-size-4-desktop ">
+                    <span
+                      className="is-size-4-mobile is-size-4-desktop animation-bar  has-text-centered-mobile"
+                      ref={textRef}
+                    >
                       Hi, I'm Davi Ribeiro (luminuszz)
                     </span>
                     <a
@@ -39,8 +45,10 @@ const Home: NextPage = () => {
                       />
                     </a>
                   </p>
-                  <p className="my-1"> - I'm a full stack developer </p>
-                  <p className="my-1 is-flex is-vcentered">
+                  <p className="my-1 is-size-4-mobile is-size-4-desktop has-text-centered-mobile">
+                    - I'm a full stack developer
+                  </p>
+                  <p className="my-1 is-flex is-vcentered is-size-5-desktop  is-size-4-mobile is-size-4-desktop has-text-centered-mobile">
                     React , React Native, Nodejs
                     <FaReact className="mx-3" />
                     <GrNode />
@@ -51,7 +59,7 @@ const Home: NextPage = () => {
 
               <div className="level-right">
                 <figure className="image">
-                  <img className="rounded" src={profile} alt="Profile" />
+                  <img className="is-rounded" src={profile} alt="Profile" />
                 </figure>
               </div>
             </div>
@@ -63,10 +71,12 @@ const Home: NextPage = () => {
         <div className="hero-body">
           <div className="container">
             <div className="title has-text-centered">
-              <h3 className="has-text-white">About me</h3>
+              <h3 className="has-text-white is-size-4 is-size-2-mobile is-size-3-tablet">
+                About me
+              </h3>
             </div>
             <div className="description-about">
-              <p className="is-size-4 is-family-secondary has-text-centered">
+              <p className="is-size-5 is-size-4-tablet is-size-4-mobile is-family-secondary has-text-centered has-text-justify-tablet ">
                 Web and mobile developer passionate about javascript and full of
                 good stories and good coffee: 3
               </p>
@@ -74,6 +84,57 @@ const Home: NextPage = () => {
           </div>
         </div>
       </section>
+
+      <section className="hero section-stack mt-6">
+        <div className="hero-body">
+          <div className="container">
+            <div className="title has-text-centered">
+              <h3 className="has-text-white  is-size-3 is-size-2-tablet is-size-2-mobile">
+                My Stack
+              </h3>
+            </div>
+            <div className="columns is-centered is-vcentered is-multiline mt-6 is-family-secondary">
+              <div className="column is-2 is-4-tablet">
+                <FaReact />
+                <h3 className="is-size-4 has-text-white hat-has-text-weight-medium mt-4 ">
+                  React.js
+                </h3>
+              </div>
+              <div className="column is-2 is-4-tablet">
+                <GrNode />
+                <h3 className="is-size-4 has-text-white hat-has-text-weight-medium mt-4 ">
+                  Node.js
+                </h3>
+              </div>
+              <div className="column is-2 is-4-tablet">
+                <FaReact />
+                <h3 className="is-size-4 has-text-white hat-has-text-weight-medium mt-4 ">
+                  React Native
+                </h3>
+              </div>
+              <div className="column is-2 is-4-tablet">
+                <DiMongodb />
+                <h3 className="is-size-4 has-text-white hat-has-text-weight-medium mt-4 ">
+                  MongoDB
+                </h3>
+              </div>
+              <div className="column is-2 is-4-tablet">
+                <DiPostgresql />
+                <h3 className="is-size-4 has-text-white hat-has-text-weight-medium mt-4 ">
+                  PostgreSQL
+                </h3>
+              </div>
+              <div className="column is-2 is-4-tablet">
+                <SiTypescript />
+                <h3 className="is-size-4 has-text-white hat-has-text-weight-medium mt-4 ">
+                  Typescript
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </>
   );
 };
