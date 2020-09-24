8a73a92924745d4182e8e22f76f6ad8f5751cd8a
luminuszz <luminuszz43@gmail.com>
2020-09-23 21:26:51 -0300

    mobile ajusts

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -14,7 +14,11 @@ const SEO: DefaultSeoProps = {
     description:
       'luminuszz.dev, Welcome  This is my little space | Bem vindo, Esse é o meu pequeno espaço',
     images: [
-      { url: '../assets/profile.jpeg', alt: 'Davi Ribeiro | luminuszz' },
+      {
+        url:
+          'https://avatars2.githubusercontent.com/u/48535259?s=460&u=06d85ab11a1bdb43207be7c6b2a2c491a48ca0b2&v=4',
+        alt: 'Davi Ribeiro | luminuszz',
+      },
     ],
   },
 
