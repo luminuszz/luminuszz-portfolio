89c0f9c0377cd56fdef49f60b7b21cd589818a19
luminuszz <luminuszz43@gmail.com>
2020-10-04 10:51:51 -0300

    404

---
+++ src/pages_/404.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { NextPage } from 'next';
+import Link from 'next/link';
+import React from 'react';
+
+import { Button } from '../components';
+
+import '../styles/globals.scss';
+import '../styles/page404.scss';
+
+const Custom404: NextPage = () => {
+  return (
+    <main className="hero main-404">
+      <div className="hero-body">
+        <div className="container">
+          <div className="content-404">
+            <div className="title">
+              <h2>Ops...</h2>
+              <h3 className="subtitle">Parece que eu me perdi....</h3>
+              <Button>
+                <Link href="/">
+                  <a>Voltar para a home</a>
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Custom404;
